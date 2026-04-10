fb571cb60f9d90fb770043210fb03a8b7853e5a5
Boston <g00436544@atu.ie>
2026-04-10 22:27:17 +0100

    decided to use the home page from the default page instead of creating one using the generate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,32 +2,24 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
   },
   {
     path: 'home',
-    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
-  },
-  {
-    path: 'home',
-    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+    loadComponent: () => import('./home/home.page').then(m => m.HomePage),
   },
   {
     path: 'watchlist',
-    loadComponent: () => import('./pages/watchlist/watchlist.page').then( m => m.WatchlistPage)
+    loadComponent: () => import('./pages/watchlist/watchlist.page').then(m => m.WatchlistPage)
   },
   {
-    path: 'detail',
-    loadComponent: () => import('./pages/detail/detail.page').then( m => m.DetailPage)
+    path: 'detail/:id',
+    loadComponent: () => import('./pages/detail/detail.page').then(m => m.DetailPage)
   },
   {
     path: 'profile',
-    loadComponent: () => import('./pages/profile/profile.page').then( m => m.ProfilePage)
+    loadComponent: () => import('./pages/profile/profile.page').then(m => m.ProfilePage)
   },
 ];
