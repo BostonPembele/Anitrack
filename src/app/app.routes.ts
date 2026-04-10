3a3963e034e829d2cdddf97cda2951b69e0df629
Boston <g00436544@atu.ie>
2026-04-10 21:07:35 +0100

    The only two things added compared to what you had are provideHttpClient() and importProvidersFrom(IonicStorageModule.forRoot()

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,24 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'watchlist',
+    loadComponent: () => import('./pages/watchlist/watchlist.page').then( m => m.WatchlistPage)
+  },
+  {
+    path: 'detail',
+    loadComponent: () => import('./pages/detail/detail.page').then( m => m.DetailPage)
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./pages/profile/profile.page').then( m => m.ProfilePage)
+  },
 ];
