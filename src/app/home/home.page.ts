a68ca74ede89be842877c146a44694c38cfa278a
Boston <g00436544@atu.ie>
2026-04-10 22:36:13 +0100

    finally was able to display the contents of my page. now i can see the information gathered from teh api

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,73 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AnimeService } from '../services/anime';
+import { CommonModule } from '@angular/common';
+import {
+  IonHeader, IonToolbar, IonTitle, IonContent,
+  IonSearchbar, IonCard, IonCardContent, IonCardHeader,
+  IonCardTitle, IonCardSubtitle, IonGrid, IonRow, IonCol,
+  IonSpinner, IonBadge
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [
+    CommonModule, FormsModule,
+    IonHeader, IonToolbar, IonTitle, IonContent,
+    IonSearchbar, IonCard, IonCardContent, IonCardHeader,
+    IonCardTitle, IonCardSubtitle, IonGrid, IonRow, IonCol,
+    IonSpinner, IonBadge
+  ]
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+
+  topAnime: any[] = [];
+  searchResults: any[] = [];
+  searchQuery: string = '';
+  isLoading: boolean = true;
+  isSearching: boolean = false;
+
+  constructor(private animeService: AnimeService, private router: Router) {}
+
+  ngOnInit() {
+    this.loadTopAnime();
+  }
+
+  loadTopAnime() {
+    this.isLoading = true;
+    this.animeService.getTopAnime().subscribe({
+      next: (response) => {
+        this.topAnime = response.data;
+        this.isLoading = false;
+      },
+      error: () => {
+        this.isLoading = false;
+      }
+    });
+  }
+
+  onSearchChange(event: any) {
+    const query = event.detail.value;
+    if (query && query.length > 2) {
+      this.isSearching = true;
+      this.animeService.searchAnime(query).subscribe({
+        next: (response) => {
+          this.searchResults = response.data;
+          this.isSearching = false;
+        },
+        error: () => {
+          this.isSearching = false;
+        }
+      });
+    } else {
+      this.searchResults = [];
+    }
+  }
+
+  goToDetail(id: number) {
+    this.router.navigate(['/detail', id]);
+  }
 }
