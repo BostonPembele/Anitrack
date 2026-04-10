4fbf3e2026daf9fdc124999589261c87f0ef5f0c
Boston <g00436544@atu.ie>
2026-04-10 22:19:50 +0100

    added anime detail services to the services page

--- src/app/services/anime.ts
+++ src/app/services/anime.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Anime {
-  
+export class AnimeService {
+
+  private baseUrl = 'https://api.jikan.moe/v4';
+
+  constructor(private http: HttpClient) {}
+
+  getTopAnime(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/top/anime`);
+  }
+
+  searchAnime(query: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/anime?q=${query}&limit=20`);
+  }
+
+  getAnimeById(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/anime/${id}/full`);
+  }
+
+  getAnimeCharacters(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/anime/${id}/characters`);
+  }
+
+  getSeasonalAnime(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/seasons/now`);
+  }
 }
